303a49cc4781dc9c57c67b01c7e27f25fe250a37
Santiago Gómez Giraldo - Ceiba Software <santiago.gomez@MacBook-Pro-de-soporteceibalocal.local>
2021-03-01 10:51:21 -0500

    Setting up ADN Project

---
+++ ADNProjectTests/ADNProjectTests.swift
@@ -0,0 +1,30 @@
+//
+//  ADNProjectTests.swift
+//  ADNProjectTests
+//
+//  Created by Santiago Gómez Giraldo - Ceiba Software on 1/03/21.
+//
+
+import XCTest
+@testable import ADNProject
+
+class ADNProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testDummy() {
+        XCTAssertTrue(true)
+    }
+
+}
